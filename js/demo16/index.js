91c11cbae93a40b491c30e3a864f97b0d39f8126
Wissenaire-23 <127422322+Wissenaire-23@users.noreply.github.com>
2024-02-01 00:37:05 +0530

    Add files via upload

---
+++ js/demo16/index.js
@@ -0,0 +1,34 @@
+import { preloadImages } from '../utils.js'; // Utility function to preload images
+import { Slideshow } from './slideshow.js';
+
+const slides = document.querySelector('.slides');
+const slideshow = new Slideshow(slides);
+var leftButton = document.getElementById("leftButton");
+var rightButton = document.getElementById("rightButton");
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        slideshow.prev();
+    }
+});
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        slideshow.next();
+    }
+});
+
+document.querySelector('.slides-nav__item--prev').addEventListener('click', () => slideshow.prev());
+document.querySelector('.slides-nav__item--next').addEventListener('click', () => slideshow.next());
+// Initialize the GSAP Observer plugin
+Observer.create({
+    type: 'wheel,touch,pointer',
+    onDown: () => slideshow.prev(),
+    onUp: () => slideshow.next(),
+    // invert the mouse wheel delta
+    wheelSpeed: -1,
+    tolerance: 10
+});
+
+// Preload all images. Once all images are preloaded, remove the 'loading' class from the body.
+preloadImages('.slide__img').then(() => document.body.classList.remove('loading'));
+
+
